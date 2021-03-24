b75f00a865f451c577d83e9e0f4a81fb79ecea32
Cindana Putri Ramadhani <cindana_pr@apps.ipb.ac.id>
2021-03-24 10:03:35 +0700

    [Cindana] Update penambahan peta

--- application/views/data_umum/js/mapJs.php
+++ application/views/data_umum/js/mapJs.php
@@ -17,5 +17,131 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 	id: 'mapbox/streets-v11'
 }).addTo(mymap);
 
+
+var roadMutant = L.gridLayer.googleMutant({
+			maxZoom: 24,
+			type:'roadmap'
+	});
+
+	map.addLayer(Layer);
+
+	var myStyle2 = {
+	    "color": "#ffff00",
+	    "weight": 1,
+	    "opacity": 0.9
+	};
+
+	function getColorAtribut(KODE){
+		for(i=0;i<dataAtribut.length;i++){
+			var data=dataAtribut[i];
+			if(data.kd_atribut==KODE){
+				return data.warna_atribut;
+			}
+		}
+	}
+
+	function popUp(f,l){
+	    var html='';
+	    if (f.properties){
+	    	html+='<table>';
+	    	html+='<tr>';
+		    	html+='<td colspan="3"><img src="<?=base_url('assets/icon-map.png')?>" width="100%"></td>';
+	    	html+='</tr>';
+	    	html+='<tr>';
+		    	html+='<td>Provinsi</td>';
+		    	html+='<td>:</td>';
+		    	html+='<td>'+f.properties['PROVINSI']+'</td>';
+	    	html+='</tr>';
+	    	html+='<tr>';
+		    	html+='<td>Atribut</td>';
+		    	html+='<td>:</td>';
+		    	html+='<td>'+f.properties['Atribut']+'</td>';
+	    	html+='</tr>';
+	    	html+='</table>';
+	    	html+='<a href="<?=site_url('Atribut')?>" target="_BLANK">'
+	    			+'<button  class="btn btn-info btn-sm" ><i class="fa fa-info"></i> Info</button></a>';
+	        l.bindPopup(html);
+	        l.bindTooltip(f.properties['Atribut'],{
+	        	permanent:true,
+	        	direction:"center",
+	        	className:"no-background"
+	        });
+	    }
+	}
+
+
+// legend
+
+	function iconByName(name) {
+		return '<i class="icon" style="background-color:'+name+';border-radius:50%"></i>';
+	}
+
+
+	var baseLayers = [
+		{
+			name: "OpenStreetMap",
+			layer: Layer
+		},
+		{	
+			name: "OpenCycleMap",
+			layer: L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png')
+		},
+		{
+			name: "Outdoors",
+			layer: L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png')
+		},
+		{
+			name:'Satelite Google',
+			layer : L.gridLayer.googleMutant({
+				maxZoom: 24,
+				type:'satellite'
+			})
+		},
+		{
+			name: "Roadmap Google",
+			layer: roadMutant
+		}
+	];
+
+	for(i=0;i<dataAtribut.length;i++){
+		var data=dataAtribut[i];
+		var layer={
+			name: data.nm_atribut,
+			icon: iconByName(data.warna_atribut),
+			layer: new L.GeoJSON.AJAX(["<?=base_url()?>assets/unggah/geojson/"+data.geojson_atribut],
+				{
+					onEachFeature:popUp,
+					style: function(feature){
+						var KODE=feature.properties.KODE;
+						return {
+							"color": getColorAtribut(KODE),
+						    "weight": 1,
+						    "opacity": 1
+						}
+
+					},
+				}).addTo(mymap)
+			}
+		layersAtribut.push(layer);
+	}
+
+	var overLayers = [{
+		group: "Layer Atribut",
+		layers: layersAtribut
+	}
+	];
+
+	var panelLayers = new L.Control.PanelLayers(baseLayers, overLayers,{
+		collapsibleGroups: true
+	});
+
+	mymap.addControl(panelLayers);
+	mymap.addControl( new L.Control.Compass({
+		position:'topleft',
+		autoActive:true,
+		showDigit:true
+	}) );
+
+
 	
 </script>
--- application/views/data_umum/mapView.php
+++ application/views/data_umum/mapView.php
@@ -1,3 +1,8 @@
+<?=content_open('Data Umum')?>
+<?=content_welcome('John Doe')?>
+<?=content_umum('BAKAUHENI - TERBANGGI BESAR')?>
+<?=content_close()?>
+
 <?=content_open()?>
-  <div id="map"><div>
+  <div id="mapid"><div>
 <?=content_close()?>
